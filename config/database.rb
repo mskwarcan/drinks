bd106875570f7455728a8a2855661761b01569cf
mskwarcan <mike@balcomagency.com>
2010-09-17 10:10:57 -0500

    Database

--- config/database.rb
+++ config/database.rb
@@ -7,7 +7,7 @@ require 'models/Bar'
 require 'models/Special'
 
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite3://bars.db')
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/bars.db")
 
 #Create or upgrade all tables
 DataMapper.auto_upgrade!
